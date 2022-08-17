47f5d17a90370af9fddd49b1ed3f2ae235409016
Hasret Ünal <s.hasretunal@gmail.com>
2022-08-17 15:59:48 +0300

    javascript hw2 added

---
+++ javascript-hw2/script.js
@@ -0,0 +1,41 @@
+let inputDOM = document.querySelector("#task");
+let ulDOM = document.querySelector("ul");
+
+// add button
+function newElement(){
+    
+    // error toast
+    if(inputDOM.value === ""){
+        $('.error').toast("show");
+    }
+    else{
+        // success toast
+        $('.success').toast("show");
+        let li = document.createElement("li");
+        li.innerHTML = inputDOM.value;
+        ulDOM.appendChild(li);
+        inputDOM.value = "";
+
+        // close 
+        let span = document.createElement("span");
+        let txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+
+        //remove
+        span.onclick = function(){
+            this.parentElement.remove();
+        }
+
+        //checked class
+        li.onclick = function(){
+            this.classList.toggle("checked");
+        }
+        }
+}
+
+
+
+
+
